b8ad06754d29f133843114f7c4b16881ccbff075
BoxinCao <ngxmex@gmail.com>
2025-06-09 00:48:30 -0400

    Update iss.js

--- iss.js
+++ iss.js
@@ -1,15 +1,17 @@
-const needle = require('needle');
+// iss.js
 
 /**
- * Makes a single API request to retrieve the user's IP address.
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
  * Input:
- *   - A callback (to pass back an error or the IP string)
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The IP address as a string (null if error)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
  */
-const fetchMyIP = function(callback) {
-  const url = 'https://api.ipify.org?format=json';
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
   needle.get(url, (error, response, body) => {
     if (error) {
@@ -18,48 +20,14 @@ const fetchMyIP = function(callback) {
     }
 
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${JSON.stringify(body)}`;
-      callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
       return;
     }
 
-    const ip = body.ip;
-    callback(null, ip);
+    const passes = body.response;
+    callback(null, passes);
   });
 };
 
-/**
- * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
- * Input:
- *   - The ip (ipv4) address (string)
- *   - A callback (to pass back an error or the lat/lng object)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The lat and lng as an object (null if error). Example:
- *     { latitude: '49.27670', longitude: '-123.13000' }
- */
-const fetchCoordsByIP = function(ip, callback) {
-  const url = `http://ipwho.is/${ip}`;
-
-  needle.get(url, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
-
-    // ipwho.is responds with 200 even on errors, so check 'success'
-    if (!body.success) {
-      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
-      callback(Error(message), null);
-      return;
-    }
-
-    const { latitude, longitude } = body;
-    callback(null, { latitude, longitude });
-  });
-};
-
-module.exports = {
-  fetchMyIP,
-  fetchCoordsByIP
-};
+// Don't need to export the other functions since we are not testing them right now.
+module.exports = { fetchISSFlyOverTimes };
