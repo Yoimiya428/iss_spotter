0137753f4eeaf018087e3c02f13b3f176f2a2cc9
BoxinCao <ngxmex@gmail.com>
2025-06-09 00:46:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// index.js
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-// TEMP TEST (can comment out later)
-fetchMyIP((error, ip) => {
+// Replace with a valid IP or use the one from your fetchMyIP result
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  console.log('It worked! Returned IP:', ip);
+  console.log('It worked! Returned coordinates:', coordinates);
 });
+
+// EXAMPLE OUTPUT:
+// It worked! Returned coordinates: { latitude: 49.2767, longitude: -123.13 }
