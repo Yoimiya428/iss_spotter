666a2702981ee514b781ce20b7df46a2ce0be72a
BoxinCao <ngxmex@gmail.com>
2025-06-09 00:33:55 -0400

    Create iss.js

---
+++ iss.js
@@ -0,0 +1,23 @@
+const needle = require('needle');
+
+const fetchMyIP = function(callback) {
+  const url = 'https://api.ipify.org?format=json';
+
+  needle.get(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${JSON.stringify(body)}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
